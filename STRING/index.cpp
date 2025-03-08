f53ff8856e27fc8346a6706a6f2f8a3f26a56560
Muhammad Yaqoob <myaqoobbrohi467@gmail.com>
2025-03-08 17:28:01 +0500

    add

---
+++ STRING/index.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+int main(){
+//========================= switch case========================================
+
+
+int num;
+cout<<"Enter a number: ";
+cin>>num;
+switch(num){
+    case 1:
+    cout<<"One"<<endl;
+    break;
+    case 2:
+    cout<<"Two"<<endl;
+    break;
+    case 3:
+    cout<<"Three"<<endl;
+    break;
+    case 4:
+    cout<<"Four"<<endl;
+    break;
+    case 5:
+
+
+
+
+    cout<<"Five"<<endl;
+    break;
+    default:
+
+    cout<<"Invalid number"<<endl;
+    break;
+}
+//========================= switch case========================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
